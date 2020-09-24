58c17f7a3e50d530333fd9fe636eade8b744447a
Chris Bertram <bertramc@bc.edu>
2020-09-24 13:28:23 -0400

    5.6 complete

---
+++ EU/Nation.swift
@@ -0,0 +1,16 @@
+//
+//  Nation.swift
+//  EU
+//
+//  Created by Chris Bertram on 9/23/20.
+//  Copyright © 2020 Chris Bertram. All rights reserved.
+//
+
+import Foundation
+
+struct Nation {
+    var country: String
+    var capital: String
+    var usesEuro: Bool
+}
+
